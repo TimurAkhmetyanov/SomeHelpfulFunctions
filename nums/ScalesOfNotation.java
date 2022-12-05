c15d0a71ff345045537529af88182b2977b14bb7
TimurPJ <timurchik0501@gmail.com>
2022-12-05 21:48:46 +0500

    binary to decimal translate

--- nums/ScalesOfNotation.java
+++ nums/ScalesOfNotation.java
@@ -1,25 +1,25 @@
 package functions.nums;
 
 public class ScalesOfNotation  {
-    public static String decimalToBinaryString(int a) {
+    public static String decimalToBinaryString(int base2) {
         int b;
         StringBuilder num = new StringBuilder();
-        while (a != 0) {
-            b = a%2;
+        while (base2 != 0) {
+            b = base2%2;
             num.append(String.valueOf(b));
-            a = a/2;
+            base2 = base2/2;
         }
         num.reverse();
         return num.toString();
     }
 
-    public static int decimalToBinaryInt(int a) {
+    public static int decimalToBinaryInt(int base2) {
         int b;
         StringBuilder num1 = new StringBuilder();
-        while (a != 0) {
-            b = a%2;
+        while (base2 != 0) {
+            b = base2%2;
             num1.append(String.valueOf(b));
-            a = a/2;
+            base2 = base2/2;
         }
         num1.reverse();
         int num = 0;
@@ -28,5 +28,25 @@ public class ScalesOfNotation  {
             num = num * 10 + Integer.parseInt(String.valueOf(c));
         }
         return num;
+    }
+    public static int binaryToDecimal(String base10) {
+        int a = Integer.parseInt(base10);
+        if (a==0) return 0;
+        StringBuilder result = new StringBuilder();
+        while (a != 0) {
+            result.append(a & 1);
+            a >>>= 1;
+        }
+        return Integer.parseInt(result.reverse().toString());
+    }
+
+    public static int binaryToDecimal(int base10) {
+        if (base10==0) return 0;
+        StringBuilder result = new StringBuilder();
+        while (base10 != 0) {
+            result.append(base10 & 1);
+            base10 >>>= 1;
+        }
+        return Integer.parseInt(result.reverse().toString());
     }
 }
