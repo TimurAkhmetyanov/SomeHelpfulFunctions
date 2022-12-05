1fc285700e3f104865fb347296163e994b29d524
TimurPJ <timurchik0501@gmail.com>
2022-12-05 21:36:56 +0500

    ScalesOfNotation create commit

--- nums/ScalesOfNotation.java
+++ nums/ScalesOfNotation.java
@@ -1,4 +1,32 @@
 package functions.nums;
 
 public class ScalesOfNotation {
+    public static String decimalToBinaryString(int a) {
+        int b;
+        StringBuilder num = new StringBuilder();
+        while (a != 0) {
+            b = a%2;
+            num.append(String.valueOf(b));
+            a = a/2;
+        }
+        num.reverse();
+        return num.toString();
+    }
+
+    public static int decimalToBinaryInt(int a) {
+        int b;
+        StringBuilder num1 = new StringBuilder();
+        while (a != 0) {
+            b = a%2;
+            num1.append(String.valueOf(b));
+            a = a/2;
+        }
+        num1.reverse();
+        int num = 0;
+        char[] chars = num1.toString().toCharArray();
+        for (char c : chars) {
+            num = num * 10 + Integer.parseInt(String.valueOf(c));
+        }
+        return num;
+    }
 }
