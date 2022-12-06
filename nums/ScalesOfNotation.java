59b8d92623753ece1b78f415e9ed77bfb7b9cb3e
TimurPJ <timurchik0501@gmail.com>
2022-12-06 19:06:45 +0500

    ScalesOfNotation update

--- nums/ScalesOfNotation.java
+++ nums/ScalesOfNotation.java
@@ -1,25 +1,25 @@
 package functions.nums;
 
 public class ScalesOfNotation  {
-    public static String decimalToBinaryString(int base2) {
+    public static String decimalToBinaryString(int base10) {
         int b;
         StringBuilder num = new StringBuilder();
-        while (base2 != 0) {
-            b = base2%2;
+        while (base10 != 0) {
+            b = base10 % 2;
             num.append(String.valueOf(b));
-            base2 = base2/2;
+            base10 = base10 / 2;
         }
         num.reverse();
         return num.toString();
     }
 
-    public static int decimalToBinaryInt(int base2) {
+    public static int decimalToBinaryInt(int base10) {
         int b;
         StringBuilder num1 = new StringBuilder();
-        while (base2 != 0) {
-            b = base2%2;
+        while (base10 != 0) {
+            b = base10 % 2;
             num1.append(String.valueOf(b));
-            base2 = base2/2;
+            base10 = base10/2;
         }
         num1.reverse();
         int num = 0;
@@ -29,8 +29,8 @@ public class ScalesOfNotation  {
         }
         return num;
     }
-    public static int binaryToDecimal(String base10) {
-        int a = Integer.parseInt(base10);
+    public static int binaryToDecimal(String base2) {
+        int a = Integer.parseInt(base2);
         if (a==0) return 0;
         StringBuilder result = new StringBuilder();
         while (a != 0) {
@@ -40,12 +40,14 @@ public class ScalesOfNotation  {
         return Integer.parseInt(result.reverse().toString());
     }
 
-    public static int binaryToDecimal(int base10) {
-        if (base10==0) return 0;
+    public static int binaryToDecimal(int base2) {
+        if (base2 == 0) {
+            return 0;
+        }
         StringBuilder result = new StringBuilder();
-        while (base10 != 0) {
-            result.append(base10 & 1);
-            base10 >>>= 1;
+        while (base2 != 0) {
+            result.append(base2 & 1);
+            base2 >>>= 1;
         }
         return Integer.parseInt(result.reverse().toString());
     }
