ee6508dafe1d1b95d77c556df31e649980a04644
TimurPJ <timurchik0501@gmail.com>
2022-12-08 21:33:35 +0500

    Changes Commit

--- lists/SomeOutputs.java
+++ lists/SomeOutputs.java
@@ -1,19 +1,8 @@
 package functions.lists;
 
 public class SomeOutputs {
-    public static int beforeDigit (char[] line, int digit) {
-        int n = 0;
-        for (char c : line) {
-            if (c == (char)(digit + '0'))
-                return n;
-            else {
-                n = n * 10 + Integer.parseInt(String.valueOf(c));
-            }
-        }
-        return n;
-    }
 
-    public static StringBuilder beforeSymbol (String line, String symbol) {
+    public static StringBuilder beforeSymbol(String line, String symbol) {
         char[] chars = line.toCharArray();
         char ch = symbol.charAt(0);
         StringBuilder out = new StringBuilder();
